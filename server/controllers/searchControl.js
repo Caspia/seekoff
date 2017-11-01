63536b438664dee567806c2ab1abf957d8216ddc
R. Kent James <rkent@caspia.com>
2017-11-01 00:10:28 -0700

    Added Previous/Next links

--- server/controllers/searchControl.js
+++ server/controllers/searchControl.js
@@ -1,28 +1,32 @@
 /**
  * Process requests from searchs page '/search'
  */
-
 const path = require('path');
+const libPath = path.join(__dirname, '..', '..', 'lib');
+
 const html2plaintext = require('html2plaintext');
 const sanitizeHtml = require('sanitize-html');
-
-const libPath = path.join(__dirname, '..', '..', 'lib');
+const canParam = require('can-param');
 const elasticClient = require(path.join(libPath, 'elasticClient'));
-const prettyjson = require('prettyjson'); // eslint-disable-line no-unused-vars
+const prettyFormat = require('pretty-format'); // eslint-disable-line no-unused-vars
 
 const client = elasticClient.client;
 
 exports.searchGet = async function (req, res, next) {
   try {
     console.log('search term is ' + req.query.search_term);
+    console.log('from is ' + req.query.from);
     if (req.query.search_term) {
+      const parms = req.query.from
+        ? {from: parseInt(req.query.from, 10)}
+        : {};
       const searchResults = await elasticClient.search(
         client,
         'stackexchange_' + 'sepost',
         req.query.search_term,
-        {},
+        parms,
       );
-      // console.log('search result: ' + prettyjson.render(searchResults));
+      //console.log('search result: ' + prettyFormat(searchResults));
 
       let renderResults = [{title: 'No results'}]; // what we will pass to template
       // Get or fake titles
@@ -31,7 +35,7 @@ exports.searchGet = async function (req, res, next) {
       if (hitResults && hitResults.length) {
         renderResults = hitResults.map((hitResult) => {
           const renderResult = {};
-          console.log('hitResult: ' + prettyjson.render(hitResult));
+          //console.log('hitResult: ' + prettyFormat(hitResult));
           if (hitResult._source) {
             // Get a title, using Body if Title missing
             if (hitResult._source.Title) {
@@ -59,11 +63,31 @@ exports.searchGet = async function (req, res, next) {
           } else {
             renderResult.title = 'Unparsable result';
           }
-          console.log('renderResult:\n' + prettyjson.render(renderResult));
+          //console.log('renderResult:\n' + prettyFormat(renderResult));
           return renderResult;
         });
       }
-      res.render('search', {title: 'Stack Caspia offline search', renderResults});
+
+      // Next and Previous searches
+      const currentFrom = parseInt(req.query.from || '0', 10);
+      const nextFrom = currentFrom + 10;
+      const previousFrom = currentFrom - 10;
+      // encode the search term with + for spaces
+      const urlPrefix = req.protocol + '://' + req.headers.host + req.path + '?'
+             + canParam({search_term: req.query.search_term}) + '&from=';
+      const nextUrl = urlPrefix + nextFrom;
+      const previousUrl = previousFrom >= 0
+        ? urlPrefix + previousFrom
+        : null;
+
+      // Show the results
+      res.render('search', {
+        title: 'Stack Caspia offline search',
+        renderResults,
+        totalHits: searchResults.hits.total,
+        nextUrl,
+        previousUrl,
+      });
     }
   } catch (err) {
     console.log('Error in searchControl get: ' + err);
